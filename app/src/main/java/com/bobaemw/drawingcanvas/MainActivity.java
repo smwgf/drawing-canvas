7ec59214045e510d9833ffb18fb260036b8632a2
MinWoo Suh <smwgf@hanmail.net>
2017-02-06 00:07:09 +0900

    add action bar - color, brush, eraser

--- app/src/main/java/com/bobaemw/drawingcanvas/MainActivity.java
+++ app/src/main/java/com/bobaemw/drawingcanvas/MainActivity.java
@@ -1,14 +1,39 @@
 package com.bobaemw.drawingcanvas;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     CanvasView _canvasView = null;
     Button _newCanvasBtn = null;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_color) {
+            return true;
+        }else if (id == R.id.menu_brush) {
+            return true;
+        }else if (id == R.id.menu_eraser) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
